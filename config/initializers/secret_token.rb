6fb55761babf18df71a4b4cc2f831926402ab3fa
Rene Ruck <reneruck@gmail.com>
2013-10-27 11:52:21 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnChinese::Application.config.secret_key_base = '0eeb9c20cbe8f4ae76853f2ad49bd33500bd0b3f196e892ae1d7a66fd26c0aa7acaefa654b0bfd954ac015efd80298623230bf452a1e2370a43f63bb9d39e6f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LearnChinese::Application.config.session_store :cookie_store, key: '_learnChinese_session'
